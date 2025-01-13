901f9d7f401eb11cc8c89f224fee1037c67efe3b
Oleh B <oleg.busygin@gmail.com>
2025-01-13 23:41:22 +0200

    entities done

---
+++ src/main/java/bstore/bookstore/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package bstore.bookstore.repository;
+
+import bstore.bookstore.model.Book;
+import java.util.List;
+
+public interface BookRepository {
+    Book save(Book book);
+
+    List<Book> findAll();
+
+}
