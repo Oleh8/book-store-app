1389fedef742780149c95e88b35877d234bbc335
Oleh B <oleg.busygin@gmail.com>
2025-02-14 15:25:40 +0200

    added validation dto and GlobalExHandler

--- src/main/java/bstore/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/bstore/bookstore/dto/CreateBookRequestDto.java
@@ -2,31 +2,27 @@ package bstore.bookstore.dto;
 
 import bstore.bookstore.validation.Isbn;
 import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
     @NotBlank
     @Size(min = 1, max = 255)
     private String title;
-    @NotNull
     @NotBlank
     @Size(min = 1, max = 255)
     private String author;
-    @NotNull
     @NotBlank
     @Isbn
     private String isbn;
     @Size(min = 10, max = 255)
     private String description;
     private String coverImage;
-    @Min(0)
-    @Digits(integer = 10, fraction = 2)
+    @Positive
+    @Digits(integer = 7, fraction = 2)
     private BigDecimal price;
 }
--- src/main/java/bstore/bookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/bstore/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -4,10 +4,12 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -23,11 +25,21 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
     ) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", System.currentTimeMillis());
-        body.put("status", status.value());
+        body.put("status", HttpStatus.BAD_REQUEST);
         List<String> errors = ex.getBindingResult().getFieldErrors().stream()
                 .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
                 .toList();
         body.put("errors", errors);
         return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(
+            EntityNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", System.currentTimeMillis());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 }
