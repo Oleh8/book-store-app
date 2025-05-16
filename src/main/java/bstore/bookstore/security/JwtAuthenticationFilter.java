a3d5bbe1319f4dd0203a7955db6e8a8718e977dd
Oleh B <oleg.busygin@gmail.com>
2025-05-17 02:44:13 +0300

    jwt-added + comments checked ++

--- src/main/java/bstore/bookstore/security/JwtAuthenticationFilter.java
+++ src/main/java/bstore/bookstore/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package bstore.bookstore.security;
 
-import jakarta.persistence.Column;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
