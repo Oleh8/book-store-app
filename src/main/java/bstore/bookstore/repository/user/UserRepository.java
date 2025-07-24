e3b7a1e841bed24fed0971f9f1a1da61761cfce2
Oleh B <oleg.busygin@gmail.com>
2025-07-24 21:01:34 +0300

    add category

--- src/main/java/bstore/bookstore/repository/user/UserRepository.java
+++ src/main/java/bstore/bookstore/repository/user/UserRepository.java
@@ -3,7 +3,6 @@ package bstore.bookstore.repository.user;
 import bstore.bookstore.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
