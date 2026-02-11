05bbc61b55ab64e8af3cfed3bd569b20f9703d4b
olegb <java.learn.mail@gmail.com>
2026-02-11 02:39:34 +0100

    created CartItem and ShoppingCart classes with necessary annotations and fields

---
+++ src/main/java/bstore/bookstore/model/ShoppingCart.java
@@ -0,0 +1,38 @@
+package bstore.bookstore.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@SQLRestriction(value = "is_deleted=false")
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "id")
+    private User user;
+
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL)
+    private Set<CartItem> cartItems = new HashSet<>();
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
