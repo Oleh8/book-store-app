11930b84d7a4acf01102485cfcf665d16f577892
olegb <java.learn.mail@gmail.com>
2026-09-03 17:37:38 +0200

    corrected annotation value

--- src/main/java/bstore/bookstore/controller/BookController.java
+++ src/main/java/bstore/bookstore/controller/BookController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Book API")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 public class BookController {
 
     private final BookService bookService;
