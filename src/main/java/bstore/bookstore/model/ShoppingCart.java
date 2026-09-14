fb3365736c9d4779b156cca0f05142edb13f93df
olegb <java.learn.mail@gmail.com>
2026-09-14 18:58:20 +0200

    fixed according to comments

--- src/main/java/bstore/bookstore/model/ShoppingCart.java
+++ src/main/java/bstore/bookstore/model/ShoppingCart.java
@@ -30,7 +30,7 @@ public class ShoppingCart {
     @JoinColumn(name = "id")
     private User user;
 
-    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CartItem> cartItems = new HashSet<>();
 
     @Column(nullable = false, columnDefinition = "TINYINT(1)")
